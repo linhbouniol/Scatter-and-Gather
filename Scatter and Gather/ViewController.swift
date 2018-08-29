3c0792b02d0e1894fa027dd10e2f0a85d8093eb4
Linh Bouniol <paulalinh89@gmail.com>
2018-08-29 13:07:01 -0700

    Set up storyboard

---
+++ Scatter and Gather/ViewController.swift
@@ -0,0 +1,21 @@
+//
+//  ViewController.swift
+//  Scatter and Gather
+//
+//  Created by Linh Bouniol on 8/29/18.
+//  Copyright © 2018 Linh Bouniol. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    @IBAction func toggle(_ sender: Any) {
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+}
+
